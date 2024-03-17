6456d82493e1e1e1901c9881629c775fad5bfd8e
Asish Mahapatra <asishmahapatra918@gmail.com>
2024-03-18 02:22:05 +0530

    Updated Environment variables for Deployed Backend

--- src/redux/thunks/fetchPropertyConfig.ts
+++ src/redux/thunks/fetchPropertyConfig.ts
@@ -1,11 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { HotelProperties } from "../../types";
 import axios from "axios";
-import { urlGenerator } from "../../utils/util";
+import { prodUrlGenerator, urlGenerator } from "../../utils/util";
 
 export const fetchPropertyConfig = createAsyncThunk(
     'hotelProperty/fetch',
     async (): Promise<HotelProperties> => {
+        const isProduction = process.env.NODE_ENV === "production";
+        if (isProduction) {
+            const response = await axios.get(prodUrlGenerator(`${process.env.CONFIG_PATH}`))
+            return response.data as HotelProperties;
+        }
         const response = await axios.get(urlGenerator(`${process.env.CONFIG_PATH}`))
         return response.data as HotelProperties;
     }
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -2,6 +2,10 @@ export function urlGenerator(path: string) {
   return `${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}/${path}`;
 }
 
+export function prodUrlGenerator(path: string) {
+  return `${process.env.BACKEND_URL}/${path}`
+}
+
 export function parseDateString(dateString: string): Date {
   const [year, month, day] = dateString.split('-').map(Number);
   return new Date(year, month - 1, day);
