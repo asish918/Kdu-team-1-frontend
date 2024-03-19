6f53d7a783255fb700e4d3e199e5c559234dc18b
Neer Patel <neerpatel25@gmail.com>
2024-03-19 11:40:07 +0530

    test; Vitest Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
@@ -15,6 +19,12 @@ export default defineConfig(({ mode }) => {
         project: "javascript-react",
       }),
     ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+      css: true,
+    },
 
     // plugins: [react()],
 
