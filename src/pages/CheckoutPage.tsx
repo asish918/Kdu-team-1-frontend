e1f5dd2edd4f46707f8e8c3991089a904e60254c
Neer Patel <neerpatel25@gmail.com>
2024-03-29 14:35:49 +0530

    IBT-57; Checkout page and Feedback form done

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,8 +1,25 @@
 import Footer from "../components/layout/Footer";
 import Itinerary from "../components/searchpage/Itinerary";
+import React, { useState } from 'react';
+import FeedbackModal from '../components/searchpage/FeedbackModal'; 
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+
+
+const CenteredDiv = styled('div')({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '100%', 
+   });
+
+const StyledButton = styled(Button)(({ theme, mt }) => ({
+    marginTop: theme.spacing(mt),
+   }));
 
 export default function CheckoutPage() {
-    // TODO Fetch this data from Redux State (Asish)
+    // Todo 
     const itineraryData = {
         dates: 'May 9 - May 16, 2024',
         guests: '1 adult 1 child',
@@ -15,13 +32,28 @@ export default function CheckoutPage() {
         dueNow: '$0000.00',
         dueAtResort: '$0000.00',
     };
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+   
+    const handleFeedbackSubmit = (feedback: string, name: string, email: string) => {
+       console.log('Feedback submitted:', { feedback, name, email });
+       handleClose();
+       
+    };
     return (
         <>
-            <div>
-                Checkout Page
-                <Itinerary itinerary={itineraryData} />
-            </div>
-            <Footer sticky={false} />
+      <CenteredDiv>
+      <div>
+        <Itinerary itinerary={itineraryData} />
+        <StyledButton variant="outlined" onClick={handleOpen} mt={2}>
+         Open Feedback Form
+        </StyledButton>
+        <FeedbackModal open={open} handleClose={handleClose} onSubmit={handleFeedbackSubmit} />
+      </div>
+     </CenteredDiv>
+        <Footer sticky={false} />
         </>
     )
 }
