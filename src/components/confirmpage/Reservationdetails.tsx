9599c61aa70812ca155d79acb8f71aea678c1813
Neer Patel <neerpatel25@gmail.com>
2024-04-05 23:00:48 +0530

    IBT-65; Fixed Responsivness

--- src/components/confirmpage/Reservationdetails.tsx
+++ src/components/confirmpage/Reservationdetails.tsx
@@ -1,121 +1,113 @@
-
-import React, { useState } from 'react';
-import { Grid, Typography, Box, Button } from '@mui/material';
+import React from 'react';
+import { Grid, Typography, Box, Button, useTheme, useMediaQuery } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import PeopleIcon from '@mui/icons-material/People';
 import CancelRoomModal from './CancelRoomModal';
 
 // Styled components 
 const StyledBox = styled(Box)({
- display: 'flex',
- alignItems: 'center',
- justifyContent: 'space-between',
- marginBottom:'5px'
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginBottom: '5px'
 });
 
 const StyledTypography = styled(Typography)({
- marginBottom: '1px',
+  marginBottom: '1px',
 });
 
-
 const LeftDiv = styled(Grid)({
- display: 'flex',
- justifyContent: 'center',
- padding: '16px',
- borderRadius: '8px',
- overflow: 'hidden',
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '16px',
+  borderRadius: '8px',
+  overflow: 'hidden',
 });
 
 const RightDiv = styled(Grid)({
- display: 'flex',
- flexDirection: 'column',
- alignItems: 'flex-start',
- paddingLeft: '16px',
- padding: '16px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  paddingLeft: '16px',
+  padding: '16px',
 });
 
 const DateBox = styled(Box)({
- width: '105px',
- height: '106px',
- borderRadius: '5px 5px 5px 5px',
- border: '1px solid grey',
- display: 'flex',
- flexDirection: 'column',
- alignItems: 'center',
- justifyContent: 'center',
- gap: '2px',
- padding: '2px',
- marginRight:'5px',
+  width: '105px',
+  height: '106px',
+  borderRadius: '5px 5px 5px 5px',
+  border: '1px solid grey',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '2px',
+  padding: '2px',
+  marginRight: '5px',
 });
 
 const CancelButton = styled(Button)({
- color: '#006EFF',
- textTransform: 'capitalize',
- marginTop: '-2px',
- marginBottom: '1px',
+  color: '#006EFF',
+  textTransform: 'capitalize',
+  marginTop: '-2px',
+  marginBottom: '1px',
 });
 const StyledImage = styled('img')({
   width: '100%',
   height: '90%',
   objectFit: 'cover',
- });
+});
 
- const OuterStyledBox = styled(Box)({
+const OuterStyledBox = styled(Box)({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   gap: 2,
   marginBottom: '20px',
   marginTop: '10px',
- });
- const StyledPeopleIcon = styled(PeopleIcon)({
+});
+const StyledPeopleIcon = styled(PeopleIcon)({
   color: '#5D5D5D',
   marginRight: '12px',
   marginLeft: '48px',
   marginTop: '2px',
   marginBottom: '3px',
- });
- const TitleStyledTypography = styled(Typography)({
+});
+const TitleStyledTypography = styled(Typography)({
   fontWeight: 600,
   marginBottom: '10px',
- });
- const InnerStyledTypography = styled(Typography)({
+});
+const InnerStyledTypography = styled(Typography)({
   variant: 'body2',
   gutterBottom: true,
   marginBottom: '12px',
   color: '#5D5D5D',
- });
- const BottomStyledTypography = styled(Typography)({
+});
+const BottomStyledTypography = styled(Typography)({
   color: '#5D5D5D',
   variant: 'body2',
   gutterBottom: true,
- });
- const RateStyledTypography = styled(Typography)({
+});
+const RateStyledTypography = styled(Typography)({
   variant: 'body2',
-  marginTop:'4px',
- });
- const Ratesxprop = {
-  fontWeight: 'bold', 
+  marginTop: '4px',
+});
+const Ratesxprop = {
+  fontWeight: 'bold',
   fontSize: '1.1 rem',
- }
- const FirstDateStyledTypography = styled(Typography)({
-  variant:"body2",
-  fontWeight: "bold",
- });
- const SecondDateStyledTypography = styled(Typography)({
-  variant: 'body2',
- });
-
- 
-const getMonthName = (monthNumber) => {
- const date = new Date();
- date.setMonth(monthNumber - 1); 
- return date.toLocaleString('default', { month: 'long' });
 };
+const FirstDateStyledTypography = styled(Typography)({
+  variant: "body2",
+  fontWeight: "bold",
+});
+const SecondDateStyledTypography = styled(Typography)({
+  variant: 'body2',
+});
 
 const ReservationDetails = () => {
- const checkInDate = { day: '15', month: '04', year: '2024' };
- const checkOutDate = { day: '20', month: '04', year: '2024' };
+  const theme = useTheme();
+  const isSm768 = useMediaQuery(theme.breakpoints.down('sm'));
+  const isXs450 = useMediaQuery('(max-width:450px)');
 
  const [open, setOpen] = useState(false);
  const [otp, setOtp] = useState('');
@@ -134,6 +126,9 @@ const ReservationDetails = () => {
     handleClose();
  };
 
+ const checkInDate = { day: '15', month: '04', year: '2024' };
+ const checkOutDate = { day: '20', month: '04', year: '2024' };
+ 
  return (
     <>
       <StyledBox>
@@ -158,10 +153,10 @@ const ReservationDetails = () => {
       />
       </StyledBox>
       <Grid container spacing={2}>
-        <LeftDiv item xs={3}>
+        <LeftDiv item xs={12} md={3} sm={6}>
           <StyledImage src="https://picsum.photos/200/200" alt="Package" />
         </LeftDiv>
-        <RightDiv item xs={9}>
+        <RightDiv item xs={12} md={9} sm={6}>
           <OuterStyledBox>
             <DateBox>
               <FirstDateStyledTypography>Check-in</FirstDateStyledTypography>
@@ -175,31 +170,39 @@ const ReservationDetails = () => {
             </DateBox>
           </OuterStyledBox>
           <TitleStyledTypography variant="h5" gutterBottom>
-           <strong>$150 Dining Credit Package</strong>
+            <strong>$150 Dining Credit Package</strong>
           </TitleStyledTypography>
           <InnerStyledTypography>
-           Spend $10 every night you stay and earn $150 on dining
-           <br />credit at the resort.
+            Spend $10 every night you stay and earn $150 on dining
+            <br />credit at the resort.
           </InnerStyledTypography>
           <Grid container spacing={2} alignItems="center">
-            <Grid item xs={6}>
+            <Grid item xs={12} sm={6}>
               <BottomStyledTypography>
-               Copy explaining the cancellation policy, if applicable.
+                Copy explaining the cancellation policy, if applicable.
               </BottomStyledTypography>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={12} sm={6}>
               <RateStyledTypography align="right" sx={Ratesxprop}>
-              $XXX/night total
+                $XXX/night total
               </RateStyledTypography>
             </Grid>
           </Grid>
         </RightDiv>
       </Grid>
     </>
- );
+  );
 };
 
 export default ReservationDetails;
+
+// Function to get month name
+const getMonthName = (monthNumber) => {
+  const date = new Date();
+  date.setMonth(monthNumber - 1);
+  return date.toLocaleString('default', { month: 'long' });
+};
+
 
 
 
